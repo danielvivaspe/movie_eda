d02dfea49497664102abb0d5190b394148159422
danielvivaspe <danielvivaspe@outlook.com>
2022-02-03 10:16:00 +0100

    Fixed streamlit cloud errors

--- presentation/phases/phase1.py
+++ presentation/phases/phase1.py
@@ -1,7 +1,9 @@
 import streamlit as st
+from PIL import Image
 
 def phase1():
-    st.image('img/eda.png', use_column_width='auto')
-    st.markdown("""
+    image = Image.open('./img/eda.png')
+    st.image(image, use_column_width='auto')
+    st.markdown("""da
                 <br/><div style="text-align: center; font-size: 90px">¿Por qué películas?</div>
             """, unsafe_allow_html=True)
